1d08a9e982cfd51d1fed10a625f8b959050ec727
Vinothasakthi <vinothasakthi414@gmail.com>
2025-04-26 14:43:41 +0530

    Update pen.js

--- pen.js
+++ pen.js
@@ -23,7 +23,7 @@ const products = [
   
   let cartCount = 0;
   let cartItems = [];
-
+  
   function applyPriceFilter() {
     const min = parseFloat(document.getElementById('minPrice').value) || 0;
     const max = parseFloat(document.getElementById('maxPrice').value) || Infinity;
@@ -47,11 +47,13 @@ const products = [
         <h3>${product.name}</h3>
         <p>$${product.price.toFixed(2)}</p>
         <button onclick="addToCart(${product.id})">Add to Cart</button>
+        <button class="buy-now-btn" onclick="buynow(${product.id})">Buy now</button>
+      
       `;
+      //for buy now
       container.appendChild(div);
     });
   }
-
   function renderProducts() {
     const container = document.getElementById('products');
     container.innerHTML = '';
@@ -64,7 +66,9 @@ const products = [
         <h3>${product.name}</h3>
         <p>$${product.price.toFixed(2)}</p>
         <button onclick="addToCart(${product.id})">Add to Cart</button>
+        <button onclick="buynow(${product.id})">Buy Now</button>            
       `;
+      //for buy now
       container.appendChild(div);
     });
   }
@@ -75,7 +79,17 @@ const products = [
     cartCount++;
     document.getElementById('cart-count').innerText = cartCount;
   }
-  
+  //for boy now
+  function buynow(id) {
+    const product = products.find(p => p.id === id);
+    const selectedProduct = {
+      name: product.name,
+      price: product.price,
+      imgUrl: product.img
+    };
+    localStorage.setItem('selectedProduct', JSON.stringify(selectedProduct));
+    window.location.href = 'p.html'; //for buy now
+  }
   function openCartModal() {
     const modal = document.getElementById('cartModal');
     const list = document.getElementById('cartItems');
@@ -110,4 +124,6 @@ const products = [
   });
   
   renderProducts();
+  
+
   
